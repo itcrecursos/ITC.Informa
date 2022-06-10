d3b895e3d97d43d7fc65d376788aef472d08ed9d
sker <sukerrojas2@gmail.com>
2022-06-10 12:33:09 -0500

    mejoras

---
+++ 2022-DATOS/instaladores-servicios/updateUserTokenClaveToken.sql
@@ -0,0 +1,5 @@
+
+update tbl_cliente_parametros_inter set txt_valor_parametro='WsC0nexMIBALON@', txt_valor_parametro2='0deA8eD+JDsxmYoyAPsWqg=='
+where num_cod_cliente=99 and cod_parametro='023';
+update tbl_cliente_parametros_inter set txt_valor_parametro='WsC0nexPOWJMP@', txt_valor_parametro2='ZbTaNWMh+4ws5V/Nsi8q8Q=='
+where num_cod_cliente=252 and cod_parametro='023';
